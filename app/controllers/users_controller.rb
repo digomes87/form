cfa8f2fad7ab522ed4ad1e9d4ec871a3150e0f59
yochiwarez <yochiwarez@gmail.com>
2019-11-13 13:19:42 -0500

    added edit user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,36 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update]
+
   def new
-    @r = User.new
+    @user = User.new
   end
 
   def create
     @r = User.create(user_params)
 
-   if @r
-    redirect_to new_user_path
-   else
-    render :new
-   end
+    if @r
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user.update(user_params)
   end
 
   private
+
   def user_params
+    # params.permit(:username, :email, :password)
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def edit; end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
